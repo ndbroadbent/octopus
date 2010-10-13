73db13fbb79d1edeac8806ead38de3730f4c399c
Nathan B <nathan.f77@gmail.com>
2010-10-13 14:38:11 +0800

    Fixed require path

--- octopus_test.rb
+++ octopus_test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # Demonstrates the 'Octopus' class for reading RFID tags.
-require 'octopus'
+require File.join(File.dirname(__FILE__), 'octopus')
 
 o = Octopus.new "/dev/ttyUSB0", 9600, 14
 puts "--- Polling for RFID tags ---"
